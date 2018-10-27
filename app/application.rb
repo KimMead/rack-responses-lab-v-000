eb30e9b2c8f40e42d9b3a31532850309f3dddda7
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-10-27 01:57:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    num_1 = Kernel.rand(1..12)
+    num_2 = Kernel.rand(13..24)
 
-
+    if num_1 
     resp.write "Good Morning!"
+    
+    else
     resp.write "Good Aternoon!"
+    end
+    resp.finish 
   end 
 end 
