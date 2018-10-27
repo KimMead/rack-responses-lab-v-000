a855750c98cfbb494bfe0a677f939d44246b3581
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-10-27 02:01:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.now.hour 
+    num_1 = Kernel.rand(1..12)
+    num_2 = Kernel.rand(13..24)
 
-    if time <= 12 
+    if num_1 
     resp.write "Good Morning!"
+    
     else
-    resp.write "Good Aternoon!"
+    resp.write "Good Afternoon!"
     end
-    resp.finish
-  end
-end
+    resp.finish 
+  end 
+end 
