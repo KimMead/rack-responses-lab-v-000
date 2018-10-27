9347cd6654cb44f356d2d672e56086118ff0ee4c
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-10-27 01:58:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,12 @@ class Application
     num_1 = Kernel.rand(1..12)
     num_2 = Kernel.rand(13..24)
 
-    if num_1 
+    if num_1
     resp.write "Good Morning!"
-    
+
     else
     resp.write "Good Aternoon!"
     end
-    resp.finish 
-  end 
-end 
+    resp.finish
+  end
+end
